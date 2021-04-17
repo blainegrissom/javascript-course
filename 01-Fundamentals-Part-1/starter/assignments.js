b425dc382bd106da1fc8f8efb8d48fe8e0227377
gamerguyquote <blainegrissom1@gmail.com>
2021-04-17 09:04:05 -0400

    Completed "Lecture: Values and Variables" of the course

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,3 @@
+let country = "USA";
+let continent = 'North America';
+let population = "300 million";
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,19 @@
+let js = 'amazing';
+console.log(40+8+23+1-10);
+
+console.log("Blaine");
+console.log(22);
+
+let firstName = "Blaine";
+console.log(firstName);
+
+let blaine_grissom = "BG";
+let $function = 27;
+
+let person = 'blaine';
+let PI = 3.1415;
+
+let myFirstJob = 'Bus boy';
+let myCurrentJob = 'Programmer';
+
+console.log(myFirstJob);
