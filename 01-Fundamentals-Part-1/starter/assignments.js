cbe091a980a5b5d8331d3be891ee54365e277d6e
gamerguyquote <blainegrissom1@gmail.com>
2021-04-18 19:18:11 -0400

    Completed "Lecture: Basic Operators"

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,7 +1,7 @@
 // Lecture: Values and Variables
 let country = "USA";
 let continent = 'North America';
-let population = "300 million";
+let population = 300000000;
 
 // Lecture: Data Types
 const isIsland = false;
@@ -15,3 +15,15 @@ console.log("Variable 'population' is type: " + typeof population);
 // Lecture: let, const, and var
 language = "English";
 //isIsland = true;
+
+// Lecture: Basic Operators
+let halfPopulation = (population/2);
+console.log(population++);
+
+const finlandPopulation = 6000000;
+console.log(population>finlandPopulation);
+
+const avgCountryPop = 33000000;
+console.log(population>avgCountryPop);
+
+let description = 'Portugal is in Europe, and its 11 million people speak portuguese.';
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -40,7 +40,6 @@ console.log(year);
 console.log(typeof year);
 
 console.log(typeof null);
-*/
 
 let age = 30;
 age = 31;
@@ -54,3 +53,33 @@ job = 'teacher';
 
 lastName = 'Grissom';
 console.log(lastName);
+*/
+
+// Math Operators
+const now = 2037;
+const ageBlaine = now - 1998;
+const ageSarah = now - 2018;
+console.log(ageBlaine, ageSarah);
+
+// 2 ** 3 means 2 to the power of 3 (or 2^3)
+console.log(ageBlaine * 2, ageBlaine / 10, 2 ** 3);
+const firstName = 'Blaine';
+const lastName = 'Grissom';
+console.log(firstName + " " + lastName);
+console.log(firstName, lastName);
+
+// Assignment Operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10
+x *= 4; // x = x * 4
+x++; // x = x + 1;
+x--; // x = x - 1;
+console.log(x);
+
+// Comparison Operators
+console.log(ageBlaine > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+console.log(ageBlaine === ageSarah);
+
+const isFullAge = ageSarah >= 18;
+console.log(now - 1991 > now - 2018);
