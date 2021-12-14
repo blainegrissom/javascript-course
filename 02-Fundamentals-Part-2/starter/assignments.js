b459d6e2a0218d724d37481e2bb1983b1e710f60
bgrissom <blainegrissom1@gmail.com>
2021-12-14 12:39:24 -0500

    Completed "Coding Challenge 1"

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -1,17 +1,19 @@
 // Lecture: Functions
 function describeCountry(country, population, capitalCity) {
-    const GIVEN_COUNTRY = `${country} has ${population} people and it's capital city is ${capitalCity}.`;
-    return GIVEN_COUNTRY;
+  const GIVEN_COUNTRY = `${country} has ${population} people and it's capital city is ${capitalCity}.`;
+  return GIVEN_COUNTRY;
 }
 
-console.log(describeCountry('United States of America','300 million','Washington D.C.'));
-console.log(describeCountry('Canada','38 million','Ottawa'));
-console.log(describeCountry('Japan','126 million','Tokyo'));
+console.log(
+  describeCountry("United States of America", "300 million", "Washington D.C.")
+);
+console.log(describeCountry("Canada", "38 million", "Ottawa"));
+console.log(describeCountry("Japan", "126 million", "Tokyo"));
 
 // Lecture: Function Declarations vs Expressions
 function percentageOfWorld1(population) {
-    console.log('This is being done using a function declaration');
-    return (population/7900)*100;
+  console.log("This is being done using a function declaration");
+  return (population / 7900) * 100;
 }
 const CHINA = percentageOfWorld1(1441);
 console.log(CHINA);
@@ -23,9 +25,9 @@ const CANADA = percentageOfWorld1(38);
 console.log(CANADA);
 
 const percentageOfWorld2 = function (population) {
-    console.log('This is being done using a function expresssion');
-    return (population/7900)*100;
-}
+  console.log("This is being done using a function expresssion");
+  return (population / 7900) * 100;
+};
 
 console.log(percentageOfWorld2(1441));
 console.log(percentageOfWorld2(300));
@@ -33,19 +35,19 @@ console.log(percentageOfWorld2(38));
 
 // Lecture: Arrow Function
 const percentageOfWorld3 = (population) => {
-    console.log('This is being done using an arrow function.');
-    return (population/7900)*100; 
-}
+  console.log("This is being done using an arrow function.");
+  return (population / 7900) * 100;
+};
 console.log(percentageOfWorld3(1441));
 console.log(percentageOfWorld3(300));
 console.log(percentageOfWorld3(38));
 
 // Lecture: Functions Calling Other Functions
-const describePopulation = (country,population) => {
-    const percentage = percentageOfWorld1(population);
-    return `${country} has a population of ${population}. This is roughly ${percentage}% of the world's population.`;
-}
+const describePopulation = (country, population) => {
+  const percentage = percentageOfWorld1(population);
+  return `${country} has a population of ${population}. This is roughly ${percentage}% of the world's population.`;
+};
 
-console.log(describePopulation('China', 1141));
-console.log(describePopulation('USA', 300));
-console.log(describePopulation('Canada', 38));
+console.log(describePopulation("China", 1141));
+console.log(describePopulation("USA", 300));
+console.log(describePopulation("Canada", 38));
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,25 +1,25 @@
-'use strict';
+"use strict";
 
 /*
 let hasDriversLicense = false;
 const passTest = true;
 
 if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log('I can drive.');
+if (hasDriversLicense) console.log("I can drive.");
 
 // const interface = 'Audio';
 // const private = 534;
 
 function logger() {
-    console.log('My name is Blaine');
+  console.log("My name is Blaine");
 }
 
 // calling / running / invoking the function
 logger();
 
 function fruitProcessor(apples, oranges) {
-    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-    return juice;
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
 }
 
 const appleJuice = fruitProcessor(5, 0);
@@ -32,47 +32,93 @@ const age1 = calcAge1(1998);
 
 // Function declaration
 function calcAge1(birthYear) {
-    return 2021 - birthYear;
+  return 2021 - birthYear;
 }
 
 console.log(age1);
 
 // Function Expression
 const calcAge2 = function (birthYear) {
-    return 2021 - birthYear;
-}
+  return 2021 - birthYear;
+};
 
 const age2 = calcAge2(1998);
 
-console.log(age1,age2);
+console.log(age1, age2);
 
 // Arrow Function
-const calcAge3 = birthYear => 2037 - birthYear;
+const calcAge3 = (birthYear) => 2037 - birthYear;
 const age3 = calcAge3(1998);
 console.log(age3);
 
 const yearsUntilRetirement = (birthYear, firstName) => {
-    const age = 2021-birthYear;
-    const retirement = 65 - age;
-    // return retirement;
-    return `${firstName} retires in ${retirement} years.`;
-}
+  const age = 2021 - birthYear;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years.`;
+};
 
-console.log(yearsUntilRetirement(1998,`Blaine`));
-console.log(yearsUntilRetirement(1980,`Bob`));
-*/
+console.log(yearsUntilRetirement(1998, `Blaine`));
+console.log(yearsUntilRetirement(1980, `Bob`));
 
 // Function Calling Other Functions
-function cutFruitPieces (fruit) {
-    return fruit*4;
+function cutFruitPieces(fruit) {
+  return fruit * 4;
 }
 
 function fruitProcessor(apples, oranges) {
-    const applePieces = cutFruitPieces(apples);
-    const orangePieces = cutFruitPieces(oranges);
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
 
-    const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
-    return juice;
+  const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+  return juice;
 }
 
-console.log(fruitProcessor(2,3));
+console.log(fruitProcessor(2, 3));
+*/
+////////////////////////////////////////////////////
+// Coding Challenge #1
+/*
+
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new
+gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 scores is calculated (so
+one average score per team).
+A team only wins if it has at least double the average score of the other team.
+Otherwise, no team wins!
+Your tasks:
+1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+2. Use the function to calculate the average for both teams
+3. Create a function 'checkWinner' that takes the average score of each team
+as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner
+to the console, together with the victory points, according to the rule above.
+Example: "Koalas win (30 vs. 13)"
+4. Use the 'checkWinner' function to determine the winner for both Data 1 and
+Data 2
+5. Ignore draws this time
+
+Test data:
+Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+
+Hints:
+To calculate average of 3 values, add them all together and divide by 3
+To check if number A is at least double number B, check for A >= 2 * B.
+Apply this to the team's average scores
+*/
+const team1 = "Dolphins";
+const team2 = "Koalas";
+const calcAverage = (score1, score2, score3) => {
+  return (score1 + score2 + score3) / 3;
+};
+const checkWinner = (avgTeam1, avgTeam2) => {
+  if (avgTeam2 >= 2 * avgTeam1) {
+    return `${team2} wins with a score of ${avgTeam2} to ${avgTeam1}.`;
+  } else if (avgTeam1 >= 2 * avgTeam2)
+    return `${team1} wins with a score of ${avgTeam1} to ${avgTeam2}.`;
+  else return "Neither team wins.";
+};
+
+const avgTeam1 = calcAverage(85, 54, 41);
+const avgTeam2 = calcAverage(23, 34, 27);
+console.log(checkWinner(avgTeam1, avgTeam2));
