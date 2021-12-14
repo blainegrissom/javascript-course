7cbf713e8e069b8daadffbf1ed1bb8eb138ce810
bgrissom <blainegrissom1@gmail.com>
2021-12-14 16:52:28 -0500

    Completed "Coding Challenge #2"

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -164,7 +164,6 @@ const ages = [
   calcAge(years[years.length - 1]),
 ];
 console.log(ages);
-*/
 
 // Lecture: Basic Array Operations (Methods)
 // Add Elements
@@ -191,3 +190,41 @@ console.log(friends.includes("Steven"));
 console.log(friends.includes("Bob"));
 console.log(friends.includes("23"));
 if (friends.includes("Peter")) console.log("You have a friend called Peter");
+*/
+
+////////////////////////////////////////////////////
+// Coding Challenge #2
+/*
+
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of
+the bill if the bill value is between 50 and 300, and if the value is different, the tip is
+20%.
+Your tasks:
+1. Write a function 'calcTip' that takes any bill value as an input and returns
+the corresponding tip, calculated based on the rules above (you can check out
+the code from first tip calculator challenge if you need to). Use the function
+type you like the most. Test the function using a bill value of 100
+2. And now let's use arrays! So create an array 'bills' containing the test data
+below
+3. Create an array 'tips' containing the tip value for each bill, calculated from
+the function you created before
+4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+
+Test data: 125, 555 and 44
+
+Hint: Remember that an array needs a value in each position, and that value can
+actually be the returned value of a function! So you can just call a function as array
+values (so don't store the tip values in separate variables first, but right in the new
+array)
+*/
+
+const calcTip = (bill) => (bill >= 50 && bill <= 300 ? 0.15 : 0.2);
+console.log(calcTip(100));
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const total = [
+  bills[0] + bills[0] * tips[0],
+  bills[1] + bills[1] * tips[1],
+  bills[2] + bills[2] * tips[2],
+];
+console.log(bills, tips, total);
